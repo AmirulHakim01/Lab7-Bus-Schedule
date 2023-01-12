ee9281766e34de551199c6587e3859ce2e7821ae
AmirulHakim01 <107103890+AmirulHakim01@users.noreply.github.com>
2023-01-12 12:40:40 +0800

    Add files via upload

---
+++ src/main/java/com/example/busschedule/BuaScheduleApplication.kt
@@ -0,0 +1,6 @@
+import android.app.Application
+import com.example.busschedule.database.AppDatabase
+
+class BusScheduleApplication : Application() {
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
+}
